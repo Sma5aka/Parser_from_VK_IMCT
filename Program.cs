b6a7ea3eafc17dc13c259de98c935fc14250c596
Sma5aka <54479462+Sma5aka@users.noreply.github.com>
2022-06-18 16:14:29 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace parser
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.parse();
+        }
+        private async Task parse()
+        {
+            string from = "https://api.vk.com/method/users.get";
+            string response = await client.GetStringAsync("https://api.vk.com/method/users.get?user_id=a.borisov1994&v=5.131");
+            Console.WriteLine(response);
+        }
+    }
+}
